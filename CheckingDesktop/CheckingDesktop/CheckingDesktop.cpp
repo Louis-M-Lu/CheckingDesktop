9e2eb28b16dafeb89a2754e1dd12f7f665e055e5
Louis-M-Lu <50077719+Louis-M-Lu@users.noreply.github.com>
2021-01-12 12:28:40 +0800

    Add files via upload

--- CheckingDesktop/CheckingDesktop/CheckingDesktop.cpp
+++ CheckingDesktop/CheckingDesktop/CheckingDesktop.cpp
@@ -1,10 +1,20 @@
 ﻿#include <fstream>
 #include <windows.h>
+#include <string>
+#include <cstring>
 #include <iostream>
+#include <stdio.h>
+#include <io.h>
+//#pragma comment( linker, "/subsystem:windows /entry:mainCRTStartup" )	//后台运行
 using namespace std;
 
-string path = "C:/Users/dell/Desktop"; //桌面位置，需要修改
-int files;
+int na = 0;	//存名字变量 
+string path = "D:/test1"; //桌面位置，需要修改
+									   //学校：C:/Users/Administrator/Desktop
+string ToPath = "D:/test";	//目标位置，需要修改
+int Files;	//统计文件数
+int BefFiles = 3;	//原来的文件数，需要修改
+string FilesName[1001] = { "" };	//存文件名
 
 LPCWSTR stringToLPCWSTR(std::string orig)
 {
@@ -19,12 +29,14 @@ LPCWSTR stringToLPCWSTR(std::string orig)
 
 int CountFiles()
 {
+	path = "D:/test1";
 	path = path + "/*.*";
 	HANDLE hFind;
 	WIN32_FIND_DATA dataFind;
 	BOOL bMoreFiles = TRUE;
 	int iCount = 0;//统计文件数的变量
 
+	
 	//m_strDir就是你要指定的路径
 	hFind = FindFirstFile(stringToLPCWSTR(path), &dataFind);//找到路径中所有文件
 
@@ -42,15 +54,43 @@ int CountFiles()
 	return iCount;
 }
 
+void FindFiles()
+{
+	//目标文件夹路径
+	path = "D:/test1";
+	std::string inPath = path + "/*.*";//遍历文件夹下的所有文件
+	//用于查找的句柄
+	long handle;
+	struct _finddata_t fileinfo;
+	//第一次查找
+	handle = _findfirst(inPath.c_str(), &fileinfo);
+	if (handle == -1)
+		return ;
+	do
+	{
+		//找到的文件的文件名
+		//printf("%s\n", fileinfo.name);
+		FilesName[na++] = fileinfo.name;
+
+	} while (!_findnext(handle, &fileinfo));
+
+	_findclose(handle);
+}
+
 int main()
 {
 
 	while (true)
 	{
-		files = CountFiles();
-		cout << files << endl;
-		Sleep(1000);
+		Files = CountFiles();
+		cout << Files << endl;
+		if (Files >= BefFiles)
+		{
+			FindFiles();
+			
+		}
 
+		Sleep(1000);
 	}
 
 }
